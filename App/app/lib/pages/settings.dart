b754c412b17a6c2ca9fda4b4ea0b5d7ab4c11dba
JonathanLu2005 <jonathanlu2005@yahoo.com>
2026-02-13 19:15:25 +0000

    T95 - Settings page UI

--- App/app/lib/pages/settings.dart
+++ App/app/lib/pages/settings.dart
@@ -7,6 +7,11 @@ import '../pages/lockSettings.dart';
 import '../pages/accessibilitySettings.dart';
 import '../services/TTS.dart';
 
+const headerStyle = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.bold
+);
+
 class SettingsPage extends StatefulWidget { 
   const SettingsPage({super.key}); 
 
@@ -31,53 +36,86 @@ class _SettingsPageState extends State<SettingsPage> {
       bottomNavigationBar: const AppNavigationBar(currentPage: 1),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: GridView.count(  
-          crossAxisCount: 3,
-          crossAxisSpacing: 12,
-          mainAxisSpacing: 12,
-          children: [
-            Tiles(
-              icon: Icons.laptop,
-              label: "Model",
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const ModelSettingsPage(),),
-                );
-              },
+
+        child: ListView(
+          children: [ 
+            const Text("Detection", style: headerStyle),
+            const SizedBox(height: 8),
+            GridView.count(
+              crossAxisCount: 3,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              children: [ 
+                Tiles(
+                  icon: Icons.laptop,
+                  label: "Model",
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const ModelSettingsPage(),),
+                    );
+                  },
+                ),
+              ],
             ),
 
-            Tiles(
-              icon: Icons.notifications,
-              label: "Alarms",
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const NotificationSettingsPage(),),
-                );
-              },
+            const SizedBox(height: 20),
+            const Text("Mitigation", style: headerStyle),
+            const SizedBox(height: 8),
+            GridView.count(
+              crossAxisCount: 3,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              children: [
+                Tiles(
+                  icon: Icons.notifications,
+                  label: "Alarms",
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const NotificationSettingsPage(),),
+                    );
+                  },
+                ),
+
+                Tiles(
+                  icon: Icons.lock_person_rounded,
+                  label: "Locking",
+                  onTap: () {
+                    Navigator.push( 
+                      context, 
+                      MaterialPageRoute(builder: (_) => const LockSettingsPage(),),
+                    );
+                  },
+                ),
+              ],
             ),
 
-            Tiles(
-              icon: Icons.lock_person_rounded,
-              label: "Locking",
-              onTap: () {
-                Navigator.push( 
-                  context, 
-                  MaterialPageRoute(builder: (_) => const LockSettingsPage(),),
-                );
-              },
-            ),
-
-            Tiles(
-              icon: Icons.visibility,
-              label: "Guidance",
-              onTap: () {
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(builder: (_) => const AccessibilitySettingsPage(),),
-                );
-              },
+            const SizedBox(height: 20),
+            const Text("Accessibility",style: headerStyle),
+            const SizedBox(height: 8),
+            GridView.count(
+              crossAxisCount: 3,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              children: [
+                Tiles(
+                  icon: Icons.visibility,
+                  label: "Guidance",
+                  onTap: () {
+                    Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (_) => const AccessibilitySettingsPage(),),
+                    );
+                  },
+                ),
+              ],
             ),
           ],
         ),
